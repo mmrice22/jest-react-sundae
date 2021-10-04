c369289248ad3e00e35824d17a0d96f118512ab6
Melissa Rice <melissa.rice@perficient.com>
2021-10-04 10:14:40 -0400

    test for catching AlertBanner passes

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -3,19 +3,23 @@ import { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOptions";
+import AlertBanner from "../common/AlertBanner";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
+
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => {
-        //TODO: handle error response
-      });
+      .catch((error) => setError(true));
   }, [optionType]);
 
-  // TODO: replace 'null' with Topping option when Available
+  if (error) {
+    return <AlertBanner />;
+  }
+
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
   const optionItems = items.map((item) => (
